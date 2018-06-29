7c8f6de30c48e78c6d41bc5d8af4d3490e01da75
Stanislav Chernichkin <schernichkin@gmail.com>
2018-06-29 17:36:09 +0300

    int

--- src/main/scala/enki/dataset/DataFrameSyntax.scala
+++ src/main/scala/enki/dataset/DataFrameSyntax.scala
@@ -1,4 +1,5 @@
-package enki.dataset
+package enki
+package dataset
 
 import org.apache.spark.sql._
 
@@ -7,6 +8,11 @@ trait DataFrameSyntax {
   implicit class DataFrameExtensions(dataFrame: DataFrame) {
     def fillna(value: Any): DataFrame = {
       functions.fillna(dataFrame, value)
+    }
+
+
+    def diff(other: DataFrame, keyColumns: Seq[String]): DataFrame = {
+      functions.diff(dataFrame, other, keyColumns)
     }
   }
 
--- src/main/scala/enki/dataset/functions.scala
+++ src/main/scala/enki/dataset/functions.scala
@@ -9,19 +9,14 @@ private[enki] object functions {
     dataFrame.select(dataFrame.columns.map(colName => coalesce(dataFrame(colName), lit(value)).as(colName)): _*)
   }
 
-  def diff(l: DataFrame, r: DataFrame, keyColumns: Seq[String]): DataFrame = {
-    val diffStatus = "diff_status"
-    val addedStatus = "+++"
-    val removedStatus = "---"
-    val updatedStatus = "->"
+  def diff(original: DataFrame, `new`: DataFrame, keyColumns: Seq[String]): DataFrame = {
+    val diffStatusColName = "diff_status"
+    val addedStatus = "added"
+    val removedStatus = "removed"
+    val updatedStatus = "updated"
 
-    //TODO:
-    // 1. найти новые записи
-    // 2. найти станые записи
-    // 3. найти записи, у которых изменились значения.
-
-    val added = keyColumns.map(l(_).isNull).reduce(_ and _)
-    val removed = keyColumns.map(r(_).isNull).reduce(_ and _)
+    val added = keyColumns.map(original(_).isNull).reduce(_ and _)
+    val removed = keyColumns.map(`new`(_).isNull).reduce(_ and _)
 
     def diffStatus(l: Column, r: Column): Column =
       when(added, addedStatus)
@@ -29,17 +24,18 @@ private[enki] object functions {
         .when(not(l <=> r), updatedStatus)
 
     def colDiff(name: String): Column = struct(
-      l(name).as("old"),
-      r(name).as("new"),
-      diffStatus(l(name), r(name)).as(diffStatus)
+      original(name).as("old"),
+      `new`(name).as("new"),
+      diffStatus(original(name), `new`(name)).as(diffStatusColName)
     ).as(name)
 
+    def rowStatus: Column =
+      when(added, addedStatus)
+        .when(removed, removedStatus)
+        .when(not(original.schema.map(col => original(col.name) <=> `new`(col.name)).foldLeft(lit(true))(_ and _)), updatedStatus)
 
-    old
-      .join(other, keyColumns, "outer")
-    //  .select()
-
-
-    ???
+    original.join(`new`, keyColumns, "outer")
+      .select(original.schema.map(c => colDiff(c.name)): _*)
+      .withColumn(diffStatusColName, rowStatus)
   }
 }
