44a1ebf5fa5f4fcbd488518d9ccf2b148e658fc5
Stanislav Chernichkin <schernichkin@gmail.com>
2018-08-06 22:52:26 +0300

    GH-3 (#24)
    * Metadata-driven dataframe cast

---
+++ enki/src/test/scala/enki/StageTest.scala
@@ -0,0 +1,22 @@
+package enki
+
+import cats.implicits._
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+
+import scala.collection.JavaConversions._
+
+case class DecimalPrecisionTestData(@decimalPrecision(38, 12) a: Option[BigDecimal])
+
+class StageTest extends EnkiTestSuite {
+  "ReadAction" should {
+    "handle non-default decimal's scale and precision in strict mode" in {
+
+      val schema = StructType(Array(StructField(name = "a", dataType = DecimalType(38, 12))))
+
+      sparkSession.sqlContext.createDataFrame(Seq(Row(BigDecimal(10))), schema).write.saveAsTable("default.DecimalPrecisionTestData")
+      val r = enki.read[DecimalPrecisionTestData](schemaName = "default", tableName = "DecimalPrecisionTestData", strict = true)
+      r.foldMap(stageCompiler).apply(sparkSession).collect() shouldBe Array(DecimalPrecisionTestData(a = BigDecimal(10)))
+    }
+  }
+}
