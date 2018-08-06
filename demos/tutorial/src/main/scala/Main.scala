cb3779c8a6025ac1a38de8aa5b16f987d7b0be37
dbugakov <you@example.com>
2018-08-06 19:37:23 +0300

    fix

--- demos/tutorial/src/main/scala/Main.scala
+++ demos/tutorial/src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 import com.monovore.decline.CommandApp
 import enki._
+import enki.tests.DatabaseState
 import org.apache.spark.sql._
 
 object Main extends CommandApp(
@@ -9,8 +10,12 @@ object Main extends CommandApp(
     buildActionGraph("root", UserDatabase.program),
     {
       val session = SparkSession.builder().master(s"local").getOrCreate()
-      SourceDatabase.createDemoTables(session)
-      UserDatabase.createDatabase(session)
+      //      SourceDatabase.createDemoTables(session)
+      //      UserDatabase.createDatabase(session)
+      new DatabaseState {
+        override protected def graph: enki.ActionGraph = buildActionGraph("root", UserDatabase.program)
+      }.emulate(session)
+
       session
     }
   ))
--- enki/src/main/scala/enki/package.scala
+++ enki/src/main/scala/enki/package.scala
@@ -1,27 +1,7 @@
-import cats.~>
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 
 package object enki
   extends AllModules {
   type SparkAction[A] = SparkSession => A
-
-
-  def findSourceStage:ActionGraph => Set[enki.ReadAction[_]] = graph =>
-    graph.linearized
-      .flatMap(name => stageReads(graph(name)))
-      .map(action => (s"${action.schemaName}.${action.tableName}", action))
-      .toSet
-      .filter(m => !graph.linearized.contains(m._1)).map(_._2)
-
-
-  def generateStateOfDatabase: (StageAction[_], SparkSession) => Unit =
-    (action, session) =>
-      action match {
-        case readAction: ReadAction[t] =>
-          if (!session.catalog.databaseExists(readAction.schemaName)) session.sql(s"create database ${readAction.schemaName}")
-          val encoder = ExpressionEncoder[t]()(readAction.tag)
-          session.emptyDataset[t](encoder).write.saveAsTable(s"${readAction.schemaName}.${readAction.tableName}")
-      }
 
 }
