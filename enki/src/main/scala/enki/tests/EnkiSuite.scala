a50e37f62657bc780fee75ff5603f032df7d7e16
dbugakov <you@example.com>
2018-08-07 16:55:28 +0300

    need fix cast

--- enki/src/main/scala/enki/tests/EnkiSuite.scala
+++ enki/src/main/scala/enki/tests/EnkiSuite.scala
@@ -22,22 +22,18 @@ trait EnkiSuite extends Defaults with ImplicitConversions with TestDataFrameModu
 
   protected implicit lazy val sparkSession: SparkSession = createSparkSession()
 
-
   def createEmptySources: (ActionGraph, SparkSession) => Unit = (graph, session) => {
-    sources(graph).foreach(action => createEmptySource(action, session))
+    sources(graph).foreach {
+      case action: ReadAction[t] =>
+        if (!session.catalog.databaseExists(action.schemaName)) session.sql(s"create database ${action.schemaName}")
+        session.emptyDataFrame.cast[t](action.strict)(action.tag).write.saveAsTable(s"${action.schemaName}.${action.tableName}")
+    }
   }
 
   def sources: ActionGraph => Set[ReadAction[_]] = graph => {
     val readers = graph.actions.flatMap { case (_, action) => stageReads(action) }.map(action => (s"${action.schemaName}.${action.tableName}", action)).toSet
     val writers = graph.actions.flatMap { case (_, action) => stageWrites(action) }.map(action => s"${action.schemaName}.${action.tableName}").toSet
     readers.filter { case (name, _) => !writers.contains(name) }.map { case (_, action) => action }
-  }
-
-  def createEmptySource(action: ReadAction[_], session: SparkSession): Unit = {
-    case (action: ReadAction[t], session: SparkSession) =>
-      if (!session.catalog.databaseExists(action.schemaName)) session.sql(s"create database ${action.schemaName}")
-      session.emptyDataFrame.cast[t](action.strict)(action.tag).write.saveAsTable(s"${action.schemaName}.${action.tableName}")
-    case _ => new Exception()
   }
 
 }
