f841bc0c396f4c3ee36d7133f5cb10348931eefe
Stanislav Chernichkin <schernichkin@gmail.com>
2018-08-31 22:13:05 +0300

    More friendly error message in createEmptySources

--- enki/src/main/scala/enki/testsuite/EnkiSuite.scala
+++ enki/src/main/scala/enki/testsuite/EnkiSuite.scala
@@ -4,9 +4,15 @@ import java.nio.file.Files
 
 import cats.implicits._
 import enki._
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
 trait EnkiSuite extends Defaults with ImplicitConversions {
+  private def sources: ActionGraph => Set[ReadTableAction] = graph => {
+    val readers = graph.analyze(action => stageReads(action, action => Set((action.toString, action))))
+    val writers = graph.analyze(action => stageWrites(action, action => Set(action.toString)))
+    readers.filter { case (name, _) => !writers.contains(name) }.map { case (_, action) => action }
+  }
+
   protected def createSparkSession(): SparkSession = {
     SparkSession
       .builder()
@@ -20,18 +26,21 @@ trait EnkiSuite extends Defaults with ImplicitConversions {
   protected implicit lazy val sparkSession: SparkSession = createSparkSession()
 
   def createEmptySources(graph: ActionGraph, session: SparkSession): Unit = {
-    sources(graph).foreach {
-      case action: ReadDatasetAction[t] =>
-        if (!session.catalog.databaseExists(action.schemaName)) session.sql(s"create database ${action.schemaName}")
-        session.emptyDataset[t](action.encoder).write.saveAsTable(action.toString)
+    sources(graph).foreach { action =>
+      val tableName = s"${action.schemaName}.${action.tableName}"
+      action match {
+        case readDataset: ReadDatasetAction[t] =>
+          if (!session.catalog.databaseExists(action.schemaName))
+            session.sql(s"create database ${action.schemaName}")
 
-      case _ => throw new UnsupportedOperationException("Can not create empty table from DataFrame.")
+          session.emptyDataset[t](readDataset.encoder)
+            .write
+            .mode(SaveMode.Overwrite)
+            .saveAsTable(tableName)
+
+        case _: ReadDataFrameAction =>
+          throw new UnsupportedOperationException(s"Can not create empty table $tableName of generic type Row.")
+      }
     }
-  }
-
-  def sources: ActionGraph => Set[ReadTableAction] = graph => {
-    val readers = graph.analyze(action => stageReads(action, action => Set((action.toString, action))))
-    val writers = graph.analyze(action => stageWrites(action, action => Set(action.toString)))
-    readers.filter { case (name, _) => !writers.contains(name) }.map { case (_, action) => action }
   }
 }
